03d2f77f305271a609ff0a170646062f82d61cbc
Marfoire <44646509+Marfoire@users.noreply.github.com>
2018-12-11 18:26:10 -0500

    Wall cling and wall jump added, horizontal movement fixed, slight tuning changes, input handled in update now to avoid dropping of inputs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,6 @@ public class PlayerMovement : MonoBehaviour {
     public float totalVelocity;
     public float deltaVelocity;
     public float prevVelocity;
-    public float displacement;
 
     //jump variables
     public float gravityValue;
@@ -30,6 +29,12 @@ public class PlayerMovement : MonoBehaviour {
 
     public BoxCollider2D bc;
 
+    public bool clingingToWall;
+
+    public bool jumpInputted;
+    public bool leftInputted;
+    public bool rightInputted;
+
     // Use this for initialization
     void Start()
     {
@@ -46,11 +51,19 @@ public class PlayerMovement : MonoBehaviour {
     void JumpCall()
     {
         //if the player is grounded and they press jump
-        if (Input.GetButtonDown("Jump") && grounded == true)
+        if (jumpInputted == true && grounded == true)
         {
+            jumpInputted = false;
             //apply the initial jump velocity
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpVelocity);
             //rb.MovePosition(new Vector2(rb.position.x, rb.position.y + jumpVelocity));
+        }
+        if (jumpInputted == true && clingingToWall == true)
+        {
+            jumpInputted = false;
+            totalVelocity = -totalVelocity * 6;
+            prevVelocity = -prevVelocity * 6;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpVelocity);
         }
     }
 
@@ -58,7 +71,7 @@ public class PlayerMovement : MonoBehaviour {
     public void HorizontalMovement()
     {
         //if the either of the input axis are receiving RAW input, start running some acceleration calculations (GetAxisRaw only returns 0,1,-1 based on what input is pressed)
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (leftInputted == true || rightInputted == true)
         {          
 
             storedLastInput = Input.GetAxisRaw("Horizontal");//store the input values in a vector2 for deceleration
@@ -73,13 +86,12 @@ public class PlayerMovement : MonoBehaviour {
             {
                 totalVelocity = MaxSpeed;//set the velocity to the max speed
             }
-
-            displacement = totalVelocity * Time.deltaTime;//multiply total velocity by Time.deltaTime to get the displacement for the translation call
+            
             //totalStart++; //this is for testing
         }
 
         //if there is no input being detected and the velocity isn't 0
-        else if (Input.GetAxisRaw("Horizontal") == 0 && totalVelocity != 0)
+        else if (leftInputted == false && rightInputted == false && totalVelocity != 0)
         {
             //if deceleration time is not 0
             if (DecelerationTime != 0)
@@ -92,7 +104,6 @@ public class PlayerMovement : MonoBehaviour {
             }
 
             totalVelocity = prevVelocity - deltaVelocity;//subtract the delta velocity from the previous velocity to get the current velocity value
-            displacement = totalVelocity * Time.deltaTime;//multiply total velocity by Time.deltaTime to get the displacement for the translation call
             //totalEnd++; //this is for testing
         }
 
@@ -112,7 +123,7 @@ public class PlayerMovement : MonoBehaviour {
         }
 
         //call the translate command for the x axis using vector3.right (storedLastInput provides input vector2 values that can never be 0,0)
-        rb.velocity = (new Vector2((storedLastInput * displacement), rb.velocity.y));
+        //rb.velocity = new Vector2((storedLastInput * displacement), rb.velocity.y);
 
         //set the previous velocity to the current total velocity for the next loop around with this function
         prevVelocity = totalVelocity;
@@ -131,16 +142,12 @@ public class PlayerMovement : MonoBehaviour {
 
             if (hit.collider.bounds.center.y + hit.collider.bounds.extents.y - 30 < rb.position.y - bc.bounds.extents.y && grounded != true && rb.velocity.y < -500)//if the ray is really far inside the stage piece it collided with and the velocity y is really high
             {
-                rb.position = new Vector3(rb.position.x, hit.collider.bounds.center.y + hit.collider.bounds.extents.y + bc.bounds.extents.y);//correct the palyer position to be just on top of that stage piece
+                rb.position = new Vector3(rb.position.x, hit.collider.bounds.center.y + hit.collider.bounds.extents.y + bc.bounds.extents.y);//correct the player position to be just on top of that stage piece
             }
 
 
             if (hit.collider.bounds.center.y + hit.collider.bounds.extents.y - 1 < rb.position.y - bc.bounds.extents.y && hit.collider.gameObject.tag == "Stage")//if the ray is colliding with the topside of the stage piece it connected with
             {
-                /*if (grounded == false)//and if grounded is false
-                {
-                    jumpTimerStartTime = Time.time;//start the jump timer
-                }*/
                 grounded = true;//set grounded to true
 
             }
@@ -153,6 +160,37 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
+    void CheckWallCling()
+    {
+        if(grounded == false && rb.velocity.x == 0 && (totalVelocity * storedLastInput == 180 || totalVelocity * storedLastInput == -180))
+        {
+            clingingToWall = true;
+            rb.velocity = new Vector2(0, rb.velocity.y - (gravityValue * Time.deltaTime));
+        }
+        else
+        {
+            clingingToWall = false;
+        }
+    }
+
+    private void Update()
+    {       
+        if (Input.GetButtonDown("Jump") == true)
+        {
+            jumpInputted = true;
+        }
+
+        if (Input.GetKey("left") == true || Input.GetKey("a"))
+        {
+            leftInputted = true;
+        }
+
+        if (Input.GetKey("right") == true || Input.GetKey("d"))
+        {
+            rightInputted = true;
+        }
+
+    }
 
     void FixedUpdate () {
 
@@ -161,16 +199,14 @@ public class PlayerMovement : MonoBehaviour {
         //call horizontal movement
         HorizontalMovement();
 
-        print(displacement);
+        CheckWallCling();
+
 
         //check the jump
         JumpCall();
 
         //apply gravity to shovel knight
-        rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + (gravityValue * Time.deltaTime));
-        //rb.MovePosition(new Vector2(rb.position.x, rb.position.y + (gravityValue * Time.deltaTime)));
-
-        
+        rb.velocity = new Vector2((totalVelocity * storedLastInput), rb.velocity.y + (gravityValue * Time.deltaTime));        
 
         //if the player is not grounded
         if (grounded == false)
@@ -178,6 +214,7 @@ public class PlayerMovement : MonoBehaviour {
             //clamp the velocity between the terminal velocity and the intial velocity
             Mathf.Clamp(rb.velocity.y, terminalVelocity, jumpVelocity);
         }
+
     }
 }
 
